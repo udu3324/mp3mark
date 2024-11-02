f90d59560bebc2adecb9793a3783a155363476ef
udu3324 <builder7177@gmail.com>
2024-11-01 23:50:59 -0500

    file selection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    let files
+    let fileInput
+
+    $: {
+        console.log("files", files)
+    }
+
+    function select() {
+        fileInput.click()
+    }
+</script>
+
+<div>
+    <b>mp3mark</b>
+    <p>A website that lets you easily annotate/mark up audio.</p>
+
+    <input id="fileInput" bind:this={fileInput} bind:files class="hidden"  type="file" name="file" accept="audio/mp3, audio/flac, audio/wav">
+    <br>
+    <button onclick={select}>Import</button>
+</div>
+
+<style lang="postcss">
+    
+</style>
