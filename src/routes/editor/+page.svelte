c2dd1d8cd134d7ed838383284e6909f952244d04
udu3324 <builder7177@gmail.com>
2024-11-10 22:59:59 -0600

    css for editor

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,3 +1,13 @@
-<div>
+<script>
+    import { db } from "../../lib/db.js"
+	import TopBar from "./TopBar.svelte";
+</script>
+
+<div class="w-screen h-screen bg-[#a1a5ae]">
+    <TopBar/>
     this is the editor page
 </div>
+
+<style lang="postcss">
+
+</style>
---
+++ src/routes/editor/TopBar.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Fa from "svelte-fa";
+
+</script>
+
+<div class="w-screen h-12 bg-[#7d818d] flex justify-between">
+    <div>
+       <h1>mp3mark</h1> 
+    </div>
+    <div class="bg-gray-300 w-32 text-3xl p-1 pr-2 text-center">
+       <p>1:30.40</p> 
+    </div>
+    <div>
+
+    </div>
+</div>
