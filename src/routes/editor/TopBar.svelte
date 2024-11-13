ccc6af6e99cd9c5509d26468e7f9fbc99eb44e08
udu3324 <builder7177@gmail.com>
2024-11-12 22:01:17 -0600

    better css for now

--- src/routes/editor/TopBar.svelte
+++ src/routes/editor/TopBar.svelte
@@ -3,14 +3,14 @@
 
 </script>
 
-<div class="w-screen h-12 bg-[#7d818d] flex justify-between">
-    <div>
+<div class="w-screen h-12 bg-[#7d818d] grid grid-cols-3">
+    <div class="pr-auto place-items-start">
        <h1>mp3mark</h1> 
     </div>
-    <div class="bg-gray-300 w-32 text-3xl p-1 pr-2 text-center">
-       <p>1:30.40</p> 
+    <div class="place-self-center bg-gray-300 w-32 text-3xl h-full p-1 place-items-center">
+       <p>1:30</p> 
     </div>
-    <div>
-
+    <div class="place-items-end">
+      <h1>seeeeeeeeeeeeeee</h1> 
     </div>
 </div>
