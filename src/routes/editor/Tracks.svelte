0ffe6ca345460c056c0c3a77b2a3f56311fc1b9a
udu3324 <builder7177@gmail.com>
2024-12-18 20:04:03 -0600

    add flags

--- src/routes/editor/Tracks.svelte
+++ src/routes/editor/Tracks.svelte
@@ -46,23 +46,23 @@
     function createTrack(track) {
         switch (track) {
             case "vocal":
-                tracks.push(["vocal", "bg-indigo-500", []])
+                tracks.push(["vocal", "bg-indigo-500", [], []])
 
                 break
             case "percussion":
-                tracks.push(["percussion", "bg-rose-500", []])
+                tracks.push(["percussion", "bg-rose-500", [], []])
 
                 break
             case "synth":
-                tracks.push(["synth", "bg-emerald-500", []])
+                tracks.push(["synth", "bg-emerald-500", [], []])
 
                 break
             case "guitar":
-                tracks.push(["guitar", "bg-cyan-600", []])
+                tracks.push(["guitar", "bg-cyan-600", [], []])
 
                 break
             case "section":
-                tracks.push(["section", "bg-amber-500", []])
+                tracks.push(["section", "bg-amber-500", [], []])
 
                 break
         }
@@ -80,7 +80,7 @@
         //console.log("m", mouseX, mouseY)
 	}
 
-    let markerName = ""
+    let note = ""
 
     let selectedTrackIndex
     let selectedBeatIndex
@@ -106,15 +106,45 @@
 
         selectedTrackIndex = ""
         selectedBeatIndex = -1
+    }
 
-        markerName = ""
-    } 
+    function createFlag() {
+        console.log("creating flag for track", selectedTrackIndex, selectedBeatIndex, `"${note}"`)
+
+        //add new flag array if track data was old
+        if (tracks[selectedTrackIndex][3] === undefined) {
+            tracks[selectedTrackIndex].push([])
+        }
+
+        note = note.trim()
+        note = note.replaceAll(" ", "_")
+
+        //start, note
+        const flag = [selectedBeatIndex, note]
+        tracks[selectedTrackIndex][3].push(flag)
+
+        // biome-ignore lint/correctness/noSelfAssign: make it svelte reactive
+        tracks = tracks
+
+        closeContext()
+    }
+
+    function deleteFlag(track, flag) {
+        console.log("removing flag", flag)
+        const trackIndex = tracks.indexOf(track)
+        const flagIndex = tracks[trackIndex][3].indexOf(flag)
+
+        tracks[trackIndex][3].splice(flagIndex, 1)
+        
+        // biome-ignore lint/correctness/noSelfAssign: make it svelte reactive
+        tracks = tracks
+    }
 
     function createMark() {
-        console.log("creating mark for track", selectedTrackIndex, selectedBeatIndex, `"${markerName}"`)
+        console.log("creating mark for track", selectedTrackIndex, selectedBeatIndex, `"${note}"`)
 
         //start, size, note
-        const mark = [selectedBeatIndex, 5, markerName]
+        const mark = [selectedBeatIndex, 5, note]
         tracks[selectedTrackIndex][2].push(mark)
 
         // biome-ignore lint/correctness/noSelfAssign: make it svelte reactive
@@ -166,13 +196,13 @@
                     updateMark(start, ++size)
                 }
             } else if (posInBar > 13) {
-                if (--size >= 3) {
+                if (--size >= 4) {
                     updateMark(++start, size)
                 }
             }
         } else {
             if (posInBar < 1) {
-                if (--size >= 3) {
+                if (--size >= 4) {
                     updateMark(start, size)
                 }
             } else if (posInBar > 13) {
@@ -222,10 +252,16 @@
 <div on:mousemove={handleMousemove} id="track-d" class="pt-12 pb-16" style="min-width: {trackLength}px">
     <div style="--menu-left: {menuLeftPX}px; --menu-top: {menuTopPX}px;" class="p-1 bg-black bg-opacity-30 fixed z-20 track-menu {trackMenuHidden}">
         <button on:click={closeContext} class="bg-white w-12 h-12">close</button>
-        <button on:click={createMark} class="bg-white w-12 h-12">add</button>
-        <input bind:value={markerName} type="text" maxlength="20" placeholder="set marker name" class="bg-white p-3">
+
+        <button on:click={createFlag} class="bg-white w-12 h-12">flag</button>
+        <button on:click={createMark} class="bg-white w-12 h-12 mb-1">mark</button>
+        
+        <br>
+
+        <input bind:value={note} type="text" maxlength="20" placeholder="set note" class="bg-white p-3">
     </div>
 
+    <!-- Visualizer Track -->
     <!-- svelte-ignore a11y_click_events_have_key_events -->
     <div class="flex flex-row h-28">
         <div class="sticky grid left-0 z-10 h-28 min-w-28 basis-28 shadow-2xl text-white bg-slate-500 text-6xl place-items-center place-content-center">
@@ -239,12 +275,12 @@
                 <div class="time-divider h-full flex">
                     {#each {length: ticks} as _, i}
                         {#if i % timeSigBeat === 0}
-                            <div on:click={() => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-500"></div>
+                            <div style="--padding-right: {marginRightValue}px;" class="tick bg-gray-500"></div>
                         {:else}
                             {#if i % 2 === 0}
-                                <div on:click={() => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-400"></div>
+                                <div style="--padding-right: {marginRightValue}px;" class="tick bg-gray-400"></div>
                             {:else}
-                                <div on:click={() => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-300"></div>
+                                <div style="--padding-right: {marginRightValue}px;" class="tick bg-gray-300"></div>
                             {/if}
                         {/if}
                     {/each}
@@ -253,6 +289,7 @@
         </div>
     </div>
 
+    <!-- Render Added Tracks -->
     {#each tracks as track}
         <div class="flex flex-row h-28">
             <div class="sticky grid left-0 z-10 h-28 min-w-28 basis-28 shadow-2xl text-white {track[1]} text-5xl leading-3 place-items-center place-content-center">
@@ -276,12 +313,12 @@
                 <div class="time-divider h-full flex">
                     {#each {length: ticks} as _, i}
                         {#if i % timeSigBeat === 0}
-                            <div on:click={() => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-400"></div>
+                            <div on:click={(e) => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-400"></div>
                         {:else}
                             {#if i % 2 === 0}
-                                <div on:click={() => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-300"></div>
+                                <div on:click={(e) => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-300"></div>
                             {:else}
-                                <div on:click={() => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-200"></div>
+                                <div on:click={(e) => showContext(tracks.indexOf(track), i)} style="--padding-right: {marginRightValue}px;" class="tick bg-gray-200"></div>
                             {/if}
                         {/if}
                     {/each}
@@ -309,7 +346,27 @@
                         </div>
                     </div>
                 {/each}
-                {#if track[2].length < 1}
+                {#each track[3] as flag}
+                    <div style="min-width: {trackLength}px; transform: translateY(-6.875rem); margin-bottom: -6.875rem" class="h-full flex pointer-events-none">
+                        <div style="transform: translateX({flag[0] * marginRightValue}px)" class="w-4 rounded-lg text-white pointer-events-auto">
+                            {#if flag[1].length > 0}
+                                <div class="flag {track[1]} w-full h-full rounded-tl-lg">
+                                    <button on:click={() => deleteFlag(track, flag)} class="mt-6 text-sm place-items-center w-4 h-6 border-y-2 border-black border-opacity-30"><Fa icon={faTrash}/></button>
+                                </div>
+                            {:else}
+                                <div class="flag {track[1]} w-full h-full">
+                                    <button on:click={() => deleteFlag(track, flag)} class="mt-6 text-sm place-items-center w-4 h-6 border-y-2 border-black border-opacity-30"><Fa icon={faTrash}/></button>
+                                </div>
+                            {/if}
+                            <div class="rounded-tl-lg">
+                                {#if flag[1].length > 0}
+                                    <span style="transform: translateY(-6.875rem)" class="{track[1]} rounded-tl-lg absolute bg-opacity-95 border-black border-r-2 border-b-2 border-opacity-30 pointer-events-none">{flag[1]}</span>
+                                {/if}
+                            </div>
+                        </div>
+                    </div>
+                {/each}
+                {#if track[2].length < 1 && track[3].length < 1}
                     <div style="transform: translateY(-6.875rem); margin-bottom: -6.875rem" class="h-full flex pointer-events-none place-items-center">
                         <span class="ml-4 bg-white">Click anywhere in this {track[0]} track to create a mark.</span>
                     </div>
@@ -317,7 +374,8 @@
             </div>
         </div>
     {/each}
-
+    
+    <!-- Add Track Tool -->
     <div class="flex flex-row h-28 drop-shadow-lg">
         <div class="sticky left-0 z-20 pointer-events-none {dropdown}">
             <div class="ml-[7.50rem] pt-6 m-2 mr-[-999px] flex">
@@ -359,5 +417,9 @@
     .track-menu {
         left: var(--menu-left);
         top: var(--menu-top);
+    }
+
+    .flag {
+        clip-path: polygon(0 0, 100% 0, 98% 90%, 50% 100%, 0 90%);
     }
 </style>
