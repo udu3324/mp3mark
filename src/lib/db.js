0569cdca71f84f9148a247b334a91d9577052863
udu3324 <builder7177@gmail.com>
2025-01-08 00:35:28 -0600

    fix

--- src/lib/db.js
+++ src/lib/db.js
@@ -18,19 +18,7 @@ db.on('populate', () => {
 
 export async function setDarkMode(enable) {
     try {
-        const preferences = await db.preference.toArray()
-        
-        if (preferences.length > 0) {
-            // Update the existing preference
-            await db.preference.update(preferences[0].id, { darkMode: enable })
-        } else {
-            // If no preference exists, create one
-            await db.preference.add({
-                darkMode: enable,
-                playSnap: true, // default values, adjust if needed
-                analysisEnterAction: 'flag'
-            });
-        }
+        await db.preference.update(1, { darkMode: enable })
   
         // Update the document's class
         const html = document.documentElement
